7a4d0f034bd5b6e8039db4bff7672c1c85273ba9
Kellee Massey <kdmassey2@gmail.com>
2015-05-20 20:39:19 -0400

    added mouseup and sound effects

--- js/app.js
+++ js/app.js
@@ -2,9 +2,32 @@ $(document).ready(function(){
 	$('.ryu').mouseenter(function(){
 		$('.ryu_still').hide();
 		$('.ryu_ready').show();
-	});
-	$('.ryu').mouseleave(function(){
-		$('.ryu-ready').hide();
-		$('.ryu-still').show();
-	});
+	})
+	.mouseleave(function(){
+		$('.ryu_ready').hide();
+		$('.ryu_still').show();
+		
+	})
+	.mousedown(function(){
+		playHadouken();
+		$('.ryu_ready').hide();
+		$('.ryu_throwing').show();
+		$('.hadouken').finish().show().animate(
+			  {'left': '1020px'},
+			  500,
+			  function() {
+			    $(this).hide();
+			    $(this).css('left', '520px');
+			  });
+		})
+	.mouseup(function(){
+		$('.ryu_throwing').hide();
+		$('.ryu_ready').show();
+	})
 });
+
+function playHadouken(){
+	$('#hadouken-sound') [0].volume = 0.5;
+	$('#hadouken-sound') [0].load();
+	$('#hadouken-sound') [0].play();
+}
